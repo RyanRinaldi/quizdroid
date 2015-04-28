38314268024336c33fafe110f740843460ddbeab
unknown <ryan.rinaldi12@gmail.com>
2015-04-28 01:10:28 -0700

    main activity and category description activity working

--- app/src/main/java/edu/washington/ryanr12/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/ryanr12/quizdroid/MainActivity.java
@@ -2,16 +2,41 @@ package edu.washington.ryanr12.quizdroid;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.content.Intent;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public String[] categories = {"Math", "Physics", "Marvel Super Heroes"};
+
+    private ListView categoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        categoryList = (ListView) findViewById(R.id.lstCategories);
+
+        ArrayAdapter<String> items = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categories);
+        categoryList.setAdapter(items);
+
+        categoryList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent next = new Intent(MainActivity.this, DescriptionActivity.class);
+                Log.i("MainActivity", "Firing intent " + next);
+                next.putExtra("categoryNumber", position);
+                next.putExtra("categoryArray", categories);
+                startActivity(next);
+            }
+        });
     }
 
 
